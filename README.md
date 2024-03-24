f22dd131bd2e91be209fd51be2e164c862502938
cpayment <cpayment12@gmail.com>
2024-03-24 17:55:23 -0500

    Filled in README and updated webpackconfig

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Changing-Text
+
+## Description
+This repository creates a browser-based text editor that operates as a single-page application and fulfills the criteria of a Progressive Web App (PWA). It should incorporate various data persistence techniques to ensure redundancy in case certain options are not supported by the browser. Furthermore, the text editor is capable of functioning offline.
+
+![Image](./images/JATE.png)
+
+## Link to Deployed Site
+[Deployed Site]()
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,46 +1,46 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-
+  // Database and version
+  const jateDB = await openDB("jate", 1);
+  // New transaction specifying db and privileges
+  const tx = jateDB.transaction("jate", "readwrite");
+  // Open desired object store
+  const store = tx.objectStore("jate");
+  // Pass in content
+  const request = store.put({ id: 1, value: content });
+  // Confirmation
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
 };
-
-// console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log('GET from the database');
-
-  // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('contact', 1);
-
-  // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('contact', 'readonly');
-
-  // Open up the desired object store.
-  const store = tx.objectStore('contact');
-
-  // Use the .getAll() method to get all data in the database.
+  // Database and version
+  const jateDB = await openDB("jate", 1);
+  // New transaction specifying db and privileges
+  const tx = jateDB.transaction("jate", "readonly");
+  // Open desired object store
+  const store = tx.objectStore("jate");
+  // Get all request
   const request = store.getAll();
-
-  // Get confirmation of the request.
+  // Confirmation and return
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  console.log("🚀 - data read from database", result);
+  return result.value;
 };
-
-// console.error('getDb not implemented');
 
 initdb();
